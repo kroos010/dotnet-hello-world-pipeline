faed30901533fefcbaece678f7c022cc755a79be
Kevin Kroos <kevinkroos@gmail.com>
2025-12-27 15:28:15 +0100

    Initial commit

---
+++ dotnet-hello-world-pipeline/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_hello_world_pipeline.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
